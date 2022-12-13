e447a5107d1aef0cd53db278c1b836d86e158aec
Sean Fisher <srtfisher@gmail.com>
2022-12-13 16:24:20 -0500

    Adding the plugin's extractors

---
+++ tests/extractor/test-feed-extractor.php
@@ -0,0 +1,96 @@
+<?php
+namespace Feed_Consumer\Tests\Extractor;
+
+use Feed_Consumer\Extractor\Feed_Extractor;
+use Feed_Consumer\Tests\Test_Case;
+use Mantle\Testing\Mock_Http_Response;
+
+use function Mantle\Support\Helpers\tap;
+
+/**
+ * @group extractor
+ */
+class Feed_Extractor_Test extends Test_Case {
+	public function test_extract_feed() {
+		$this->fake_request(
+			'https://alley.com/feed/',
+			Mock_Http_Response::create()
+				->with_header( 'Content-Type', 'application/rss+xml' )
+				->with_body( file_get_contents( __DIR__ . '/../fixtures/rss-feed.xml' ) )
+		);
+
+		$processor = $this->make_processor(
+			[
+				'feed_url' => 'https://alley.com/feed/',
+			]
+		);
+
+		$extractor = tap(
+			new Feed_Extractor(),
+			fn ( Feed_Extractor $extractor ) => $extractor->processor( $processor ),
+		)->run();
+
+		$data = $extractor->data();
+
+		$this->assertTrue( $data->is_xml() );
+		$this->assertStringContainsString( '<channel>', $data->body() );
+		$this->assertEmpty( $extractor->cursor() );
+	}
+
+	public function test_extract_feed_error() {
+		$this->fake_request(
+			'https://alley.com/feed/',
+			Mock_Http_Response::create()->with_status( 404 )
+		);
+
+		$processor = $this->make_processor(
+			[
+				'feed_url' => 'https://alley.com/feed/',
+			]
+		);
+
+		$extractor = tap(
+			new Feed_Extractor(),
+			fn ( Feed_Extractor $extractor ) => $extractor->processor( $processor ),
+		)->run();
+		$data      = $extractor->data();
+
+		$this->assertEmpty( $data->body() );
+		$this->assertEmpty( $extractor->cursor() );
+	}
+
+	public function test_extract_basic_auth() {
+		$_SERVER['__did_auth'] = false;
+
+		$this->fake_request(
+			'https://alley.com/feed/',
+			function ( string $url, array $args ) {
+				if ( ! empty( $args['headers']['Authorization'] ) ) {
+					$_SERVER['__did_auth'] = 'Basic ' . base64_encode( 'username:password' ) === $args['headers']['Authorization'];
+				}
+
+				return Mock_Http_Response::create()
+					->with_header( 'Content-Type', 'application/rss+xml' )
+					->with_body( file_get_contents( __DIR__ . '/../fixtures/rss-feed.xml' ) );
+			}
+		);
+
+		$processor = $this->make_processor(
+			[
+				'feed_url'      => 'https://alley.com/feed/',
+				'feed_username' => 'username',
+				'feed_password' => 'password',
+			]
+		);
+
+		$extractor = tap(
+			new Feed_Extractor(),
+			fn ( Feed_Extractor $extractor ) => $extractor->processor( $processor ),
+		)->run();
+
+		$data = $extractor->data();
+
+		$this->assertTrue( $data->is_xml() );
+		$this->assertTrue( $_SERVER['__did_auth'] );
+	}
+}
