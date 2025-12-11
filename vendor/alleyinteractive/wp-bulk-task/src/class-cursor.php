15e5588e824bf94ca258393e8995a553c749f6f5
mogmarsh <mogmarsh@users.noreply.github.com>
2025-12-11 14:57:12 +0000

    Built changes for v1.2.0 from develop 8560e815f05ea11afd978b585cfda3105135d1c4

---
+++ vendor/alleyinteractive/wp-bulk-task/src/class-cursor.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Alley\WP_Bulk_Task: Cursor class
+ *
+ * @package alleyinteractive/wp-bulk-task
+ */
+
+declare(strict_types=1);
+
+namespace Alley\WP_Bulk_Task;
+
+/**
+ * Legacy cursor class for an option cursor.
+ *
+ * @deprecated 1.1.0 Use Alley\WP_Bulk_Task\Cursor\Option_Cursor instead.
+ * @package alleyinteractive/wp-bulk-task
+ */
+class Cursor extends Cursor\Option_Cursor {}
